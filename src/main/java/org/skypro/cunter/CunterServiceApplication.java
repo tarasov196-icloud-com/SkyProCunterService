c06c2ca1fd8afcfa6c48e94325212e060d3f2498
Ilya <tarasov190@icloud.com>
2025-09-21 14:54:01 +0300

    initial commit

---
+++ src/main/java/org/skypro/cunter/CunterServiceApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.cunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CunterServiceApplication.class, args);
+	}
+
+}
